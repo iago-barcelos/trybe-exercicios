abc514ce16b22024f28bb9a5881780eb28dbda26
iago-barcelos <iago.cmpb@gmail.com>
2023-03-30 15:44:36 -0300

    resolução de exercicio

---
+++ Fundamentos/Arrays/main.js
@@ -0,0 +1,19 @@
+let contaX = 200;
+
+function adicionaValor () {
+    return contaX + ;
+}
+
+function subtraiValor (){
+    return contaX - ;
+}
+
+function taxaValor () {
+    return contaX * ;
+}
+
+function divideValor () {
+    return contaX / 
+}
+
+//O parametro de cada função deve ser substituído por um valor para concluir a operação
